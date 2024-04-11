02c08f6f2a620c62e5edda1081d83e95ac45a170
Pedro Freitas <79390915+FreitassPedro@users.noreply.github.com>
2024-04-11 15:16:44 -0300

    Upload Project

---
+++ 23. Projeto Web Services Spring boot/projeto-webservices-spring-jpahibernate/src/main/java/br/com/pedro/projetowebservicesspringjpahibernate/ProjetoWebservicesSpringJpahibernateApplication.java
@@ -0,0 +1,14 @@
+package br.com.pedro.projetowebservicesspringjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebservicesSpringJpahibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebservicesSpringJpahibernateApplication.class, args);
+	}
+
+}
+ 
