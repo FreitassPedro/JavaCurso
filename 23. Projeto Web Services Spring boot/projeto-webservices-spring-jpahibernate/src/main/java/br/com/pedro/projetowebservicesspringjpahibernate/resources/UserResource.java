b5616375dafc31805a492c1a43ea1825aa63d4e8
Pedro Freitas PC <pedrospf2018@gmail.com>
2024-04-13 21:42:28 -0300

    User Update

--- 23. Projeto Web Services Spring boot/projeto-webservices-spring-jpahibernate/src/main/java/br/com/pedro/projetowebservicesspringjpahibernate/resources/UserResource.java
+++ 23. Projeto Web Services Spring boot/projeto-webservices-spring-jpahibernate/src/main/java/br/com/pedro/projetowebservicesspringjpahibernate/resources/UserResource.java
@@ -3,13 +3,13 @@ package br.com.pedro.projetowebservicesspringjpahibernate.resources;
 import java.net.URI;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,5 +54,11 @@ public class UserResource {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deletUser(id);
         return ResponseEntity.noContent().build();
-    } 
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+        userService.updateUser(id, obj);
+        return ResponseEntity.ok().body(obj);     
+    }
 }
